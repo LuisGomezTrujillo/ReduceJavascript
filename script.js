1956a05e2dadbccee5284b2ab750085eb1863d9e
LuisGT <luisgomeztrujillo@gmail.com>
2022-10-16 18:30:48 -0500

    Prueba de acumulador de votos

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-function demo(){
-   // document.getElementById('demo').innerHTML = Date();
+function prueba(){
+   
    const votos1=document.querySelector('#opcion1');
    const votos2=document.querySelector('#opcion2');
    const votos3=document.querySelector('#opcion3');
 
-   //document.getElementById('resultado').innerHTML=parseInt(votos1.value)+parseInt(votos2.value)+parseInt(votos3.value);
-
+   
    arreglo=[
               parseInt(votos1.value), 
               parseInt(votos2.value),
@@ -15,16 +14,3 @@ function demo(){
   const resultado=arreglo.reduce((sum,elemento) => sum +elemento,0);
   document.getElementById('resultado').innerHTML=resultado; 
 }
-
-
-// const app = document.getElementById('app');
-//     const app2 = document.getElementById('app2');
-//     const totals = [1, 2, 3, 4];
-//     app.innerHTML = `
-//       <h1>Array: ${totals}</h1>
-//       `
-
-//     const rta = totals.reduce(( sum, elemnto) => sum + elemnto, 0);
-//     app2.innerHTML = `
-//       <h1>Reduce: ${rta}</h1>
-//       `
